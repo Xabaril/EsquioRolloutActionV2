b5c07e293208191bad92d0277b3ba570d4512a50
Luis Fraile <lfraile@lfraile.net>
2019-12-08 18:11:39 +0100

    First commit for esquio (V2) rollout action

---
+++ README.md
@@ -0,0 +1,25 @@
+# Esquio (V2) Github Action rollout feature
+
+With this Esquio Github action you can enable a feature in an Github Actions workflow.[Esquio](https://esquio.readthedocs.io/en/latest/).
+
+Please read [Esquio readthedocs](https://esquio.readthedocs.io/en/latest/) first to fully understand Esquio Feature Toggle package configuration and possibilities.
+
+## Parameters needed
+
+- **esquioUrl**: Url to the ESquio Api. i.e.: https://myesquioui.deployment.com
+- **esquioApiKey**: API key to authenticate to esquio. Recommended to store as [Github secret](https://help.github.com/en/github/automating-your-workflow-with-github-actions/virtual-environments-for-github-actions#creating-and-using-secrets-encrypted-variables)
+- **productName**: Name of the product to which the feature belongs.
+- **featureName**: Name of the feature to enable.
+
+## Example
+
+```YAML
+      - name: Esquio rollout
+        uses: actions/esquio-rollout
+        id: esquio-rollout
+        with:
+          esquioUrl: 'https://esquiodemoui.azurewebsites.net/'
+          esquioApiKey: ${{ secrets.apikey }}
+          productName: 'Default'
+          featureName: 'MatchScore'
+```
---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import * as core from '@actions/core';
+import url =  require('url'); 
+const https = require('https');
+
+async function run() {
+  try {
+    const esquioUrl = core.getInput('esquioUrl');
+    const esquioApiKey = core.getInput('esquioApiKey');
+    const productName = core.getInput('productName');
+    const featureName = core.getInput('featureName');
+
+    await rolloutFeature(url.parse(esquioUrl), esquioApiKey, productName, featureName);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function rolloutFeature(esquioUrl: url.UrlWithStringQuery, esquioApiKey: string, productName: string, featureName: string) {
+  const options = {
+      hostname: esquioUrl.host,
+      path: `/api/products/${productName}/features/${featureName}/Rollout?apikey=${esquioApiKey}`,
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': esquioApiKey
+      }
+  }
+  const req = https.request(options, (res: any) => {
+      if (res.statusCode === 200) {
+          console.log('Feature rollout succesful');
+      }
+
+      res.on('data', (data: any) => {
+          if (res.statusCode != 200) {
+              const responseData = JSON.parse(data);
+              core.setFailed(`Error in feature rollout ${responseData.detail} HttpCode: ${res.statusCode}`);
+          }
+      });
+  });
+  req.on('error', (error: any) => {
+    core.setFailed(error);
+  });
+
+  req.end();
+}
+
+run();

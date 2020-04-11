12aedf2f5edb0c17ebaa30b6e4cb6c8479270181
Luis Fraile <lfraile@lfraile.net>
2020-04-11 11:58:50 +0200

    Corrected values

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ const https = require('https');
 
 async function run() {
   try {
-    const esquioUrl ='https://esquiodemoui.azurewebsites.net/';
-  const esquioApiKey = 'NRGIjdU2D7HPy9H5JnQeR9MwBCY1ZdKDtDjiligIoGs=';
-  const productName = 'Default';
-    const featureName = 'MatchScore';
+    const esquioUrl = core.getInput('esquioUrl');
+    const esquioApiKey = core.getInput('esquioApiKey');
+    const productName = core.getInput('productName');
+    const featureName = core.getInput('featureName');
 
     await rolloutFeature(url.parse(esquioUrl), esquioApiKey, productName, featureName);
   } catch (error) {
